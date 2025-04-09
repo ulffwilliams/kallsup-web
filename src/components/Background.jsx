bdca3b1099b5af073a5c0070e141e8df53ef8cb4
ulffwilliams <ulffwilliam@gmail.com>
2025-04-09 22:38:47 +0200

    Add Spotify SVG logo and update shows JSON with upcoming events
    - Added a new SVG file for the Spotify logo in the images directory.
    - Created a JSON file containing details of upcoming shows, including dates, locations, venues, notes, and ticket links.

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -4,7 +4,7 @@ import '/src/scss/background.scss'
 function Background() {
   return (
     <>
-    <video src="kallsup-web/images//7.4.mp4" autoPlay muted loop id='background-video'></video>
+    <video src="./images/7.4.mp4" autoPlay muted loop id='background-video'></video>
     </>
   )
 }
